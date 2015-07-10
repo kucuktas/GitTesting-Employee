dc658509b063f720cfd17b51c88c075b6f6b14a4
kucuktas <kucuktas.mete@gmail.com>
2015-07-10 11:50:59 +0300

    testing

--- src/EmployeeTesting.java
+++ src/EmployeeTesting.java
@@ -1,10 +1,13 @@
 
-
 public class EmployeeTesting {
 
 	public static void main( String[] args ) {
-		// TODO Auto-generated method stub
 
+		SalariedEmployee salaried = new SalariedEmployee( "Salaried", "Employee", "000000000", 500 );
+		HourlyEmployee hourly = new HourlyEmployee( "Hourly", "Employee", "111111111", 50, 41 );
+		CommissionEmployee commission = new CommissionEmployee( "Commission", "Employee", "222222222", 1000, 0.1 );
+		BasePlusCommissionEmployee bpCommission = new BasePlusCommissionEmployee( "BasePlus", "Employee", "333333333",
+				1000, 0.9, 100 );
 	}
 
 }
